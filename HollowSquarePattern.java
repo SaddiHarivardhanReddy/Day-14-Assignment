ac173583573a4e4997892fc53d91a03720c6793a
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-28 21:41:51 +0530

    Hollow Square Pattern

---
+++ HollowSquarePattern.java
@@ -0,0 +1,26 @@
+package com.codegnan.patterns;
+
+import java.util.Scanner;
+
+public class HollowSquarePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the side length of the square: ");
+        int side = scanner.nextInt();
+
+        for (int i = 0; i < side; i++) { 
+            for (int j = 0; j < side; j++) { 
+               
+                if (i == 0 || i == side - 1 || j == 0 || j == side - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
+}
