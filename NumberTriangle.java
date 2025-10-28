a27514e6f0481a6461000b8bb21d68dc4afcdd19
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-28 21:42:35 +0530

    Number Triangle Pattern

---
+++ NumberTriangle.java
@@ -0,0 +1,19 @@
+package com.codegnan.patterns;
+
+import java.util.Scanner;
+
+public class NumberTriangle {
+    public static void main(String[] args) {
+    	Scanner s=new Scanner(System.in);
+        int n=s.nextInt(); 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
